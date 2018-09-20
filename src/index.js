f5a6ba00d2e32c80f2bbd92da41cee5ca0a0b6c1
lixunhuan <lixunhuan@weierai.com>
2018-09-20 19:58:51 +0800

    umd 引入直接安装

--- src/index.js
+++ src/index.js
@@ -1,40 +1,43 @@
+export let _Vue
+const install = Vue => {
+  let listener = []
+  if (install.installed && _Vue === Vue) return
+  install.installed = true
+  _Vue = Vue
+  window.addEventListener('message', (res) => {
+    const {data} = res
+    if (data && typeof data === 'object' && data.type) {
+      listener.forEach(obj => {
+        if (data.type === obj.type) {
+          obj.callback(res)
+        }
+      })
+    }
+  })
 
-export default {
-  install (Vue) {
-    let listener = []
-    window.addEventListener('message', (res) => {
-      const {data} = res
-      if (data && typeof data === 'object' && data.type) {
-        listener.forEach(obj => {
-          if (data.type === obj.type) {
-            obj.callback(res)
+  Vue.mixin({
+    created () {
+      const message = this.$options.message
+      if (message) {
+        Object.keys(message).forEach((type) => {
+          if (typeof message[type] === 'function') {
+            listener.push({
+              type,
+              callback: message[type].bind(this),
+              _uid: this._uid
+            })
           }
         })
       }
-    })
-
-    Vue.mixin({
-      created () {
-        const message = this.$options.message
-        if (message) {
-          Object.keys(message).forEach((type) => {
-            if (typeof message[type] === 'function') {
-              listener.push({
-                type,
-                callback: message[type].bind(this),
-                _uid: this._uid
-              })
-            }
-          })
-        }
-      },
-      beforeDestroy () {
-        const message = this.$options.message
-        if (message) {
-          listener = listener.filter(obj => obj._uid !== this._uid)
-          delete this.$options.message
-        }
+    },
+    beforeDestroy () {
+      const message = this.$options.message
+      if (message) {
+        listener = listener.filter(obj => obj._uid !== this._uid)
+        delete this.$options.message
       }
-    })
-  }
+    }
+  })
 }
+if (window && window.Vue)window.Vue.use(install)
+export default {install}
